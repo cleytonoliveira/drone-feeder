d0c1cc55cd06f8761d07e2899ce90546ac583446
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2022-07-05 21:57:38 -0300

    refactor(controller): change PATCH to PUT

--- src/main/java/org/futuereh/controller/DeliveryController.java
+++ src/main/java/org/futuereh/controller/DeliveryController.java
@@ -8,8 +8,8 @@ import javax.validation.Valid;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.PATCH;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -47,7 +47,7 @@ public class DeliveryController {
     return Response.status(Response.Status.CREATED).entity(service.save(deliveryDto)).build();
   }
 
-  @PATCH
+  @PUT
   @Path("/{id}")
   public Response update(@PathParam("id") Long id, @Valid DeliveryDto deliveryDto) {
     return Response.ok(service.update(deliveryDto, id)).build();

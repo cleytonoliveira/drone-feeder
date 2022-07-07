0045d13ed3379ad28200382c974a1f4c8738220b
Cleyton de Oliveira <cleytonoliveirapro@gmail.com>
2022-07-07 07:05:43 -0300

    fix(DroneController): checkstyle

--- src/main/java/org/futuereh/controller/DroneController.java
+++ src/main/java/org/futuereh/controller/DroneController.java
@@ -2,7 +2,15 @@ package org.futuereh.controller;
 
 import javax.inject.Inject;
 import javax.validation.Valid;
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.PATCH;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import org.futuereh.dto.DroneDto;
@@ -47,19 +55,28 @@ public class DroneController {
 
   @PATCH
   @Path("/{id}/withdrawal/{deliveryId}")
-  public Response withdrawal(@PathParam("id") Long id, @PathParam("deliveryId") Long deliveryId, @Valid DroneDto droneDto) {
+  public Response withdrawal(
+      @PathParam("id") Long id,
+      @PathParam("deliveryId") Long deliveryId,
+      @Valid DroneDto droneDto) {
     return Response.ok(service.withdrawal(droneDto, id, deliveryId)).build();
   }
 
   @PATCH
   @Path("/{id}/delivery/{deliveryId}")
-  public Response delivery(@PathParam("id") Long id, @PathParam("deliveryId") Long deliveryId, @Valid DroneDto droneDto) {
+  public Response delivery(
+      @PathParam("id") Long id,
+      @PathParam("deliveryId") Long deliveryId,
+      @Valid DroneDto droneDto) {
     return Response.ok(service.delivery(droneDto, id, deliveryId)).build();
   }
 
   @PATCH
   @Path("/{id}/map/{deliveryId}")
-  public Response maps(@PathParam("id") Long id, @PathParam("deliveryId") Long deliveryId, @Valid DroneDto droneDto) {
+  public Response maps(
+      @PathParam("id") Long id,
+      @PathParam("deliveryId") Long deliveryId,
+      @Valid DroneDto droneDto) {
     return Response.ok(service.maps(droneDto, id, deliveryId)).build();
   }
 
